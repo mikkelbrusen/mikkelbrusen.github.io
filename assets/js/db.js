97e6bd221c61e978e171a0f6d85465c4dafecb65
Mikkel Brusen <Mikkel_mb@hotmail.com>
2016-01-05 17:00:19 +0100

    Add gifs

--- assets/js/db.js
+++ assets/js/db.js
@@ -1,4 +1,7 @@
 $(function() {
+    //Buttons
+  $("button[name='add']").click(add);
+
   //Authentication
   var client = new Dropbox.Client({ key: "75ozb45t6z1tq24" });
   client.authenticate({interactive: false}, function(error, client) {
@@ -8,7 +11,7 @@ $(function() {
     }
     if (client.isAuthenticated()) {
       // Cached credentials are available, make Dropbox API calls.
-      console.log("success")
+      console.log("cached")
     } else {
       // show and set up the "Sign into Dropbox" button
       var button = document.querySelector("#signin-button");
@@ -20,51 +23,39 @@ $(function() {
             console.log("error");
             return;
           }
-          console.log("success")
+          button.setAttribute("class", "visible");
         });
       });
     }
   });
 
-  //Account Info
-  var AccInfoB = document.querySelector("#account-button");
-  AccInfoB.addEventListener("click", function() {
-    // The user will have to click an 'Authorize' button.
-    client.getAccountInfo(function(error, accountInfo) {
-      if (error) {
-        return showError(error);  // Something went wrong.
-      }
+  var count = 0;
 
-      alert("Hello, " + accountInfo.name + "!");
-    });
-  });
-
-  //Buttons
-  $("button[name='add']").click(add);
-  $("button[name='reset']").click(reset);
-  //Add File
+  //Add Data
   function add(){
-    console.log("clicked");
-    var form = document.getElementById('fileInfo');
-    var name = form[0].value;
-    var sub = form[1].value;
+    console.log("Add Clicked");
+    var user = $('#user').val();
+    var myClass = $('input[name="myClass"]:checked').val();
+    var theirClass = $('input[name="theirClass"]:checked').val();
+    var result = $('input[name="result"]:checked').val();
 
-    client.writeFile("/space/"+sub+"/"+name+".txt", "Hello, world!\n", function(error, stat) {
+    client.writeFile("/space/Feed/"+user+"_"+myClass+"_"+theirClass+"_"+result+"."+count, "Hello, world!\n", function(error, stat) {
       if (error) {
+        console.log("error");
         return showError(error);  // Something went wrong.
       }
+      count += 1;
+      console.log("file uploaded");
+      console.log(user);
+      console.log(myClass);
+      console.log(theirClass);
+      console.log(result);
+      console.log(count);
 
-      alert("File saved as revision " + stat.versionTag);
     });
-  }
-
-  function reset(){
-    var form1 = document.getElementById('fileInfo');
-    for (var i = form1.length - 1; i >= 0; i--) {
-      form1[i].value = "";
-    };
   }
 });
+
 
 
 
