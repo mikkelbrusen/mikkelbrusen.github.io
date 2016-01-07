4bd3b549fd6d565dce87889c7f3c525b7744f1bd
Mikkel Brusen <Mikkel_mb@hotmail.com>
2016-01-07 14:04:58 +0100

    Test live

--- assets/js/db.js
+++ assets/js/db.js
@@ -1,31 +1,30 @@
 $(function() {
-    //Buttons
-  $("button[name='add']").click(add);
 
-  //Authentication
-  var client = new Dropbox.Client({ key: "75ozb45t6z1tq24" });
-  client.authenticate({interactive: false}, function(error, client) {
-    if (error) {
-      console.log("error");
-      return;
-    }
-    if (client.isAuthenticated()) {
-      // Cached credentials are available, make Dropbox API calls.
-      console.log("cached")
-    } else {
-      client.authenticate(function(error, client) {
-        if (error) {
-          console.log("error");
-          return;
-        }
-        button.setAttribute("class", "visible");
-      });
-    }
-  });
-
+  // Variables
   var count = 0;
 
-  //Add Data
+  // Buttons
+  $("button[name='add']").click(add);
+  $("button[name='personalS']").click(getPersonalStats);
+  //$("button[name='generalS']").click(getGeneralStats);
+  //$("button[name='otherI']").click(getOtherInfo);
+
+  // Authentication
+  var client = new Dropbox.Client({ key: "75ozb45t6z1tq24" });
+  if (client.isAuthenticated()) {
+    // Cached credentials are available, make Dropbox API calls.
+    console.log("auth cached")
+  } else {
+    client.authenticate(function(error, client) {
+      if (error) {
+        console.log("auth error");
+        return;
+      }
+      console.log("auth Success")
+    });
+  }
+
+  // Add Data
   function add(){
     console.log("Add Clicked");
     var user = $('#user').val();
@@ -35,13 +34,43 @@ $(function() {
 
     client.writeFile("/space/Feed/"+user+"_"+myClass+"_"+theirClass+"_"+result+"."+count, "Hello, world!\n", function(error, stat) {
       if (error) {
-        console.log("error");
+        console.log("error uploading");
         return showError(error);  // Something went wrong.
       }
       count += 1;
       console.log("file uploaded");
     });
   }
+
+  // Get Personal Statistics
+  var pDruidS = $('#pDruidS');
+
+  function getPersonalStats(){
+    console.log("getPersonalStatsClicked");
+
+    var user = $('#login').val();
+
+    pDruidS.html("5");
+
+
+  }
+
+  function auxReadPClass(c){
+    //client.readFile
+  }
+
+  function auxReadPReset(){
+
+  }
+
+  // Get General Statistics
+
+  // Get Other Statistics
+
+  // Request Friendship
+
+  // Accept Friendship
+
 });
 
 
